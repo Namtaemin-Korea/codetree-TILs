4381f0c19ed39446dbc60c499eb2ccbe4ece21f9
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-07 19:10:15 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240107/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3, b = 5;
+    int tmp = 0;
+    tmp = a;
+    a = b;
+    b = tmp;
+    cout << a << "\n" << b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
