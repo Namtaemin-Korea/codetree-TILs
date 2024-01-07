b9709bd97a7e505905a1ed658e05931001a24493
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-07 16:04:29 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240107/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 5.26, b = 8.27;
+    cout << fixed;
+    cout.precision(3);
+    cout << a * b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
