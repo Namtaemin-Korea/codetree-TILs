87c35504633e94f2536ad1cc744ad80770369851
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-08 22:22:38 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/합과 평균/sum-and-avg.cpp
+++ 240108/합과 평균/sum-and-avg.cpp
@@ -3,7 +3,10 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << a + b << " " << double(a + b) / 2;
+    cout << a + b << " ";
+    cout << fixed;
+    cout.precision(1);
+    cout << double(a + b) / 2;
     // 여기에 코드를 작성해주세요.
     return 0;
 }
