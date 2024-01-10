e0cd8d6431ae36297c854c8f7a1dbf9054a666f2
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-10 21:48:10 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240110/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score;
+    cin >> score;
+    if (score = 100) {
+        cout << "pass";
+    }
+    else {
+        cout << "failure";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
