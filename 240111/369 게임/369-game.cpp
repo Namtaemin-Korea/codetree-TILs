a7652c85533670388a80746fb71775f3e6c77936
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-11 23:12:21 +0900

    Title: 369 게임, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/369 게임/369-game.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0) {
+            cout << '0';
+        }
+        else if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+            cout << '0';
+        }
+        else if (i / 10 == 3 || i / 10 == 6 || i / 10 == 9) {
+            cout << '0';
+        }
+        else {
+            cout << i;
+        }
+        cout << " ";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
