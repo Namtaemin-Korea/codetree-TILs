9394c791119e3162520ca761e0abcf51d02d7ba4
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-11 22:16:43 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240111/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a;
+    cin >> a;
+    if (a >= 1.0) {
+        cout << "High";
+    }
+    else if (a >= 0.5 && a < 1.0) {
+        cout << "Middle";
+    }
+    else {
+        cout << "Low";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
