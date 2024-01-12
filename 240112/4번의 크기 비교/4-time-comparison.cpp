9e41d32c5e8d2891af0b2ecfacf99891017dcc0d
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-12 22:54:59 +0900

    Title: 4번의 크기 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240112/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    cout << (a > b) << "\n";
+    cout << (a > c) << "\n";
+    cout << (a > d) << "\n";
+    cout << (a > e) << "\n";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
