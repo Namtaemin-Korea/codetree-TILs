d2d9435e53c8c58b0eb1a7a81bda594189b382c0
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-14 10:54:28 +0900

    Title: 좀 더 어려운 수학 점수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240114/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int am, ae, bm, be;
+    cin >> am >> ae >> bm >> be;
+    if (am > bm) {
+        cout << 'A';
+    }
+    else if (am < bm) {
+        cout << 'B';
+    }
+    else {
+        if (ae > be) {
+            cout << 'A';
+        }
+        else {
+            cout << 'B';
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
