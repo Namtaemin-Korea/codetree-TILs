ed4c326450ee03d03b21cdba425dc457b22104d5
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-15 20:10:55 +0900

    Title: 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240115/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a > b) {
+        if (a > c) {
+            cout << a;
+        }
+        else if (b > c) {
+            cout << b;
+        }
+        else {
+            cout << c;
+        }
+    }
+    else {
+        if (b > c) {
+            cout << b;
+        }
+        else if (a > c) {
+            cout << a;
+        }
+        else {
+            cout << c;
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
