01930a38cc426a5280c9013632c5644a743289ff
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-19 22:40:58 +0900

    Title: 증가시키며 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/증가시키며 출력하기 3/increase-and-print-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 10;
+    while (true) {
+        cout << n << " ";
+        n += 2;
+        if (n > 26)
+        break;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
