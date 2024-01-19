f243cf1358e54d3c98da87269028413ce65ce843
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-19 22:06:06 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, k = 1;
+    cin >> n;
+    while (k <= n) {
+        cout << k << " ";
+        k++;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
