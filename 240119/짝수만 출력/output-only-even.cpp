aba8a6f054cdbc278f58b36a97e4240cef534d09
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-19 22:49:09 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, n;
+    cin >> a >> b;
+    n = a;
+    while (n <= b) {
+        cout << n << " ";
+        n += 2;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
