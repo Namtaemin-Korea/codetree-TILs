78dd3321cc2ead2f00b7a3feac031297065dd447
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-19 22:35:34 +0900

    Title: 별 n개 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k = 1;
+    cin >> n;
+    while (k <= n) {
+        cout << "*\n";
+        k++;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
