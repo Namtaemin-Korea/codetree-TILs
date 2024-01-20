9aeb8dddfa7cb0fdd98e02ad27e7f35c962c3374
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-20 09:46:58 +0900

    Title: 감소시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 26;
+    while (n > 9) {
+        cout << n << " ";
+        n--;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
