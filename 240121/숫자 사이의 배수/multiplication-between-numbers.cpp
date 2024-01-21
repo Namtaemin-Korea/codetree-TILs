ca126d2598892d88b6a41309d89781badb82f324
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-21 21:04:42 +0900

    Title: 숫자 사이의 배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240121/숫자 사이의 배수/multiplication-between-numbers.cpp
+++ 240121/숫자 사이의 배수/multiplication-between-numbers.cpp
@@ -4,21 +4,21 @@ int main() {
     int a, b;
     double sum = 0;
     cin >> a >> b;
-    for (int i = 1; i <= (b / 5); i++) {
+    for (int i = (a / 5) + 1; i <= (b / 5); i++) {
         sum += (5 * i);
     }
-    for (int i = 1; i <= (b / 7); i++) {
+    for (int i = (a / 7) + 1; i <= (b / 7); i++) {
         sum += (7 * i);
     }
     if (b >= 35) {
-        for (int i = 1; i <= (b / 35); i++) {
+        for (int i = (a / 35) + 1; i <= (b / 35); i++) {
             sum -= (35 * i);
         }
     }
     cout << sum << " ";
     cout << fixed;
     cout.precision(1);
-    cout << (sum / ((b / 5) + (b / 7) - (b / 35)));
+    cout << (sum / (((b / 5) - (a / 5)) + ((b / 7) - (a / 7)) - ((b / 35) - (a / 35))));
     // 여기에 코드를 작성해주세요.
     return 0;
 }
