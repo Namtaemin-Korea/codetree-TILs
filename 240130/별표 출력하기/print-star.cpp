da0f02d94f5613ec9208838610c470f7a3bf926e
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-01-30 21:57:57 +0900

    Title: 별표 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/별표 출력하기/print-star.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= (2 * n) - 1; i++) {
+        if (i <= n) {
+            for (int j = 1; j <= i; j++) {
+                cout << '*' << ' ';
+            }
+        }
+        else {
+            for (int j = 1; j <= n - (i - n); j++) {
+                cout << '*' << ' ';
+            }
+        }
+        cout << "\n";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
