a391b69c0f7dd214a09b2cf19bf140d3a7720b9e
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-02-07 23:32:39 +0900

    Title: 좌우 지그재그 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240207/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < n; j++) {
+                cout << (j + 1) + (i * n) << ' ';
+            }
+        }
+        else {
+            for (int j = n; j > 0; j--) {
+                cout << j + (i * n) << ' ';
+            } 
+
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
