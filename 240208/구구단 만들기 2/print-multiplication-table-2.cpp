d551c463cb2363a1e1a92fd2d066b80ff1e66f1d
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-02-08 22:52:52 +0900

    Title: 구구단 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    for (int i = 2; i <= 8; i++) {
+        for (int j = b; j >= a; j--) {
+            cout << j << " * " << i << " = " << j * i;
+            if (j > a) {
+                cout << " / ";
+            }
+        }
+        i++;
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
