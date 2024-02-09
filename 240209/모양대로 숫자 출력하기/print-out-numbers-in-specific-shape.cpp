977add7f208dadffba874c35cbbcf6ebf39f2865
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-02-09 12:43:25 +0900

    Title: 모양대로 숫자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240209/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << "  ";
+        }
+        for (int j = n - i; j > 0; j-- ) {
+            cout << j << ' ';
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
