d9ad0f4bae363e1613c7f52bbf89cb336d7c7b89
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-02-10 20:28:12 +0900

    Title: 19단 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240210/19단 출력/nineteen-times-table.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 1; i <= 19; i++) {
+        for (int j = 1; j <= 19; j++) {
+            if (j % 2 == 0 || j == 19) {
+                cout << i << " * " << j << " = " << i * j << endl;
+            }
+            else {
+                cout << i << " * " << j << " = " << i * j << " / ";
+            }
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
