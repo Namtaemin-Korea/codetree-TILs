cd7b39a5085d733b5272a729429e10200a7bb306
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-02-11 17:08:24 +0900

    Title: 알파벳 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240211/알파벳 출력하기/print-alphabet.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, cnt = 65;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = n-i+1; j <= n; j++) {
+            if (cnt > 'Z')
+            cnt = 'A';
+            cout << (char)cnt;
+            cnt++;
+        }
+        cout << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
