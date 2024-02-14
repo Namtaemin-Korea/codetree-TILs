d0d31b85683154cea61a913c01852db3689abb41
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-02-14 21:56:24 +0900

    Title: 평균점수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240214/평균점수 구하기/print-average.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double arr[8], sum = 0;
+    for (int i = 0; i < 8; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    cout << sum / 8;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
