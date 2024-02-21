dc96ccd4810247ff79e926966535f565ee287c6f
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-02-21 21:58:50 +0900

    Title: 점수대 파악하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240221/점수대 파악하기/find-out-the-score-range.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[110], count_val[10] = { 0, }, cnt = 0;
+    while (true) {
+        cin >> arr[cnt];
+        count_val[(arr[cnt] / 10) - 1]++;
+        if (arr[cnt] == 0)
+        break;
+        cnt++;
+    }
+    for (int i = 9; i >= 0; i--) {
+        cout << (i + 1) * 10 << " - " << count_val[i] << endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
