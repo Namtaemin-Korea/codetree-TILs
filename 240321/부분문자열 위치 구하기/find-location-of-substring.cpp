359a111440804da09687d94a61bce9cf29e33923
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-03-21 22:51:06 +0900

    Title: 부분문자열 위치 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240321/부분문자열 위치 구하기/find-location-of-substring.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    int len1, len2;
+    len1 = str1.length();
+    len2 = str2.length();
+
+    bool y_n = true;
+
+    for (int i = 0; i < len1; i++) {
+        if (str1[i] == str2[0]) {
+            y_n = true;
+            for (int j = 0; j < len2; j++) {
+                if (str1[i + j] != str2[j]) {
+                    y_n = false;
+                }
+            }
+            if (y_n == true) {
+                cout << i;
+                break;
+            }
+        }
+    }
+
+    if (y_n == false)
+    cout << -1;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
