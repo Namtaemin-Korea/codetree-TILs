49183ab80671cae692dce823e981b411d869c75a
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-03-22 22:50:36 +0900

    Title: 문자 교체하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240322/문자 교체하기/changing-char.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string A, B;
+    cin >> A >> B;
+
+    B[0] = A[0];
+    B[1] = A[1];
+
+    cout << B;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
