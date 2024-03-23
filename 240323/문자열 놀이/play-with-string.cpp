e031f839f44798e9835c992f314db4de2487e267
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-03-23 22:39:28 +0900

    Title: 문자열 놀이, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240323/문자열 놀이/play-with-string.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s;
+    int q;
+    cin >> s >> q;
+
+    for (int i = 0; i < q; i++) {
+        int k;
+        cin >> k;
+        if (k == 1) {
+            int a, b;
+            cin >> a >> b;
+
+            char tmp;
+            tmp = s[a - 1];
+            s[a - 1] = s[b - 1];
+            s[b - 1] = tmp;
+
+            cout << s << endl; 
+        }
+        else if (k == 2) {
+            char a, b;
+            cin >> a >> b;
+
+            for (int j = 0; j < s.length(); j++) {
+                if (s[j] == a)
+                    s[j] = b;
+            }
+            
+            cout << s << endl;
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
