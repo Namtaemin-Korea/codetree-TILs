c7d49b7926c03613369f8a15a7e07d00a88caf47
Namtaemin-Korea <119743033+Namtaemin-Korea@users.noreply.github.com>
2024-06-13 23:32:53 +0900

    Title: 미는 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240613/미는 횟수/number-of-pushes.cpp
+++ 240613/미는 횟수/number-of-pushes.cpp
@@ -5,13 +5,13 @@ int main() {
     string A, B;
     int output = 0;
     cin >> A >> B;
-    for (int i = 0; i < A.length() + 1; i++) {
+    for (int i = 0; i < A.length() + 5; i++) {
         if (A != B) {
         A = A.substr(1, A.length()) + A.substr(0, 1);
         output++;
-            if (i == A.length()) {
+            if (i == A.length() + 4) {
                 cout << -1;
-                break;
+                break; 
             }
         }
         else {
